0566b4c65b91edd40e953ee0c9a6c7c563dff40e
tw1ttt3r <tw1ttt3r@gmail.com>
2020-03-23 12:27:43 -0600

    feat: implemented vimeo/player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef, AfterViewChecked } from '@angular/core';
+
+import Player from '@vimeo/player';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewChecked {
   title = 'vimeoangular';
+
+  constructor() {}
+
+  @ViewChild('vimeoframe', {static: true}) frameVideo: ElementRef;
+
+  player: any;
+
+  ngOnInit() {
+    console.log(this.frameVideo);
+    this.player = new Player(this.frameVideo.nativeElement);
+    console.log(this.player);
+  }
+
+  ngAfterViewChecked() {
+    this.player.on('play', () => {
+      console.log('play', this.player);
+    });
+  }
 }
