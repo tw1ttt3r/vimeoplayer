6703a49f8d249c2df90aa85f48d7ea870184b41d
tw1ttt3r <tw1ttt3r@gmail.com>
2020-03-24 16:20:58 -0600

    feta: implemented new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import VimeoConfig from './interfaces/VimeoConfig';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vimeoangular';
+
+  config: VimeoConfig = {
+    id: 18636045,
+    autopause: false
+  };
 }
