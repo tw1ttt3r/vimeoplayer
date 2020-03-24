a912079cc291893ca9d00d058bbe93100953fc7f
tw1ttt3r <tw1ttt3r@gmail.com>
2020-03-24 16:18:37 -0600

    feat: added VimeoConfig Interface

---
+++ src/app/interfaces/VimeoConfig.ts
@@ -0,0 +1,24 @@
+export default interface VimeoConfig {
+    id: number;
+    autopause?: boolean;
+    autoplay?: boolean;
+    background?: boolean;
+    byline?: boolean;
+    color?: string;
+    controls?: boolean;
+    dnt?: boolean;
+    height?: number;
+    loop?: boolean;
+    responsive?: boolean;
+    maxheight?: number;
+    maxwidth?: number;
+    muted?: boolean;
+    playsinline?: boolean;
+    portrait?: boolean;
+    quality?: number;
+    speed?: boolean;
+    texttrack?: string;
+    title?: boolean;
+    transparent?: boolean;
+    width?: number;
+}
